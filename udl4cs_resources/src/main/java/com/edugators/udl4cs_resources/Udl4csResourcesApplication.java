72cbfb24c96a8e273dcc3e4078ec7ef53dbce906
mliamkin <mliamkin56@gmail.com>
2024-02-27 22:41:57 -0500

    New next.js

---
+++ udl4cs_resources/src/main/java/com/edugators/udl4cs_resources/Udl4csResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.edugators.udl4cs_resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Udl4csResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Udl4csResourcesApplication.class, args);
+	}
+
+}
---
+++ udl4cs_resources/src/test/java/com/edugators/udl4cs_resources/Udl4csResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edugators.udl4cs_resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Udl4csResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
