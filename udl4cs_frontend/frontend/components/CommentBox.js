77e9a1f8bdd0ae05e2b96f6bb9209f0e2bf1b514
Jon Williams <jwilliams16@ufl.edu>
2024-03-10 22:31:35 -0400

    Add feature for comment box to dynamically change size

--- udl4cs_frontend/frontend/components/CommentBox.js
+++ udl4cs_frontend/frontend/components/CommentBox.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function CommentBox() {
+    const [comment, setComment] = useState('');
+
+    const updateCommentState = (event) => {
+        setComment(event.target.value);
+    };
+
+    const dynamicTextBoxArea = () => {
+        const lines = comment.split('\n').length;
+        return `${lines * 40}px`;
+    };
+
     return (
         <form style={{ maxWidth: '60%', margin: 'auto', padding: '20px', borderRadius: '16px', backgroundColor: '#EAEAEA', boxShadow: '0 0 10px rgba(234, 234, 234, 0.8)' }}>
             <h2 style={{ textAlign: 'left', color: '#000000', marginBottom: '20px', fontStyle: 'normal', fontWeight: '400', fontSize: '26px', lineHeight: '35px' }}>Comments</h2>
             <div className="commentBoxRow d-flex flex-column align-items-center">
-        <textarea
-            className="commentBody"
-            style={{ width: '90%', padding: '10px', borderRadius: '16px', marginBottom: '10px' }}
-            placeholder="Add a comment..."
-        ></textarea>
+                <textarea
+                    className="commentBody"
+                    style={{ width: '90%', padding: '10px', borderRadius: '16px', marginBottom: '10px', height: dynamicTextBoxArea(), resize: 'none'}}
+                    placeholder="Add a comment..."
+                    value={comment}
+                    onChange={updateCommentState}
+                ></textarea>
                 <button
                     type="submit"
                     className="postButton"
