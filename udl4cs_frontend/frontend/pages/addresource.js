4e999908c5418f9759cf9535029224824bbf03a3
mliamkin <mliamkin56@gmail.com>
2024-03-13 12:32:03 -0400

    Added Frontend elements from andrea's branch

---
+++ udl4cs_frontend/frontend/pages/addresource.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddResource from '../components/AddResource'
+
+const addresource = () => {
+    return (
+        <div>
+            <AddResource></AddResource>
+        </div>
+    )
+}
+
+export default addresource
