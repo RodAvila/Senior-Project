bd89e360d34eeb03a0ab6d3d9e3e75984ec77eee
mliamkin <mliamkin56@gmail.com>
2024-03-22 00:58:37 -0400

    Updated track with mui

--- udl4cs_frontend/frontend/pages/resources.js
+++ udl4cs_frontend/frontend/pages/resources.js
@@ -1,18 +1,22 @@
 import Link from 'next/link'
 import AddResource from "../components/AddResource"
 import ResourceCard from '../components/ResourceCard';
-import { React } from "react";
+import {React, useState} from "react";
 import Navbar from "../components/Navbar";
 import Search from '../components/Search';
+import SearchResults from '../components/SearchResults';
 
 
 
 export default function Resources() {
+    const [results, setResults] = useState([]);
     return (
         <>
             <Navbar></Navbar>
-            <Search></Search>
-
+            <div className=" py-5 border-bottom">
+                <Search setResults = {setResults}></Search>
+                <SearchResults results = {results}></SearchResults>
+            </div>
             <div className="container my-3 ">
 
                 <ResourceCard></ResourceCard>
