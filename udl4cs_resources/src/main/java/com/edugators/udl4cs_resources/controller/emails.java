71e30994838ed551907182be9233903e79f593b6
jackTobin01 <61993410+jackTobin01@users.noreply.github.com>
2024-03-25 13:37:54 -0400

    Update emails.java
    Changed to Google SMTP API to send emails, established udl4cs.no.reply@gmail.com as temporary email sender

--- udl4cs_resources/src/main/java/com/edugators/udl4cs_resources/controller/emails.java
+++ udl4cs_resources/src/main/java/com/edugators/udl4cs_resources/controller/emails.java
@@ -7,36 +7,44 @@ import java.io.*;
 public class emails {
     //File -> Full file attached if reasonable size-wise, alt. first page/screenshot
     //Title -> title of proposed file
-    public static void sendEmail(File attach, String title) {
-        //static info, to update login info update these strings
-        String username = "199cce88a02901";
-        String password = "f95fd211967780";
-        
+
+    //bot account = udl4cs.no.reply@gmail.com
+    //bot password = RumpledTurtle49
+    static String  d_email = "udl4cs.no.reply@gmail.com",
+            d_password = "mjyw whii oece unwt",
+            d_port = "465",
+            d_host = "smtp.gmail.com",
+            m_to = "udl4cs.no.reply@gmail.com",
+            m_subject = "Test File";
+    public void sendEmail(File attach, String title) {
+        //API used to send email
+        Properties prop = new Properties();
+
+        String  from = d_email,
+                host = d_host,
+                port = d_port,
+                to = m_to,
+                subject = m_subject;
+
+        prop.put("mail.smtp.user", from);
+        prop.put("mail.smtp.host", host);
+        prop.put("mail.smtp.port", port);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", port);
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        prop.put("mail.smtp.socketFactory.fallback", "false");
+
+        SecurityManager security = System.getSecurityManager();
+
         try {
-            //API used to send email
-            Properties prop = new Properties();
-            prop.put("mail.smtp.auth", true);
-            prop.put("mail.smtp.starttls.enable", "true");
-            prop.put("mail.smtp.host", "sandbox.smtp.mailtrap.io");
-            prop.put("mail.smtp.port", "25");
-            prop.put("mail.smtp.ssl.trust", "sandbox.smtp.mailtrap.io");
-
-            //Needs permanent address, static login information
-            Session session = Session.getInstance(prop, new Authenticator(){
-                @Override
-                protected PasswordAuthentication
-                getPasswordAuthentication() {
-                    return new PasswordAuthentication(username, password);
-                }
-            });
-
+            Authenticator auth = new SMTPAuthenticator();
+            Session session = Session.getInstance(prop, auth);
             Message msg = new MimeMessage(session);
-            //Needs permanent email address (do-no-reply)
-            msg.setFrom(new InternetAddress("temp@gmail.com"));
-            //check whether this needs to be dynamic or static
-            //Also needs permanent address
-            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("tempTo@gmail.com"));
-            msg.setSubject("A new file has been suggested");
+
+            msg.setFrom(new InternetAddress(from));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            msg.setSubject(subject);
 
             Multipart totBody = getMultipart(attach, title);
 
@@ -45,6 +53,12 @@ public class emails {
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static class SMTPAuthenticator extends javax.mail.Authenticator {
+        public PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(d_email, d_password);
         }
     }
 
@@ -58,16 +72,28 @@ public class emails {
         Multipart Body = new MimeMultipart();
         Body.addBodyPart(textBodyPart);
 
+
         MimeBodyPart attBodyPart = new MimeBodyPart();
         //May remove, depends on results of testing with videos
         attBodyPart.attachFile(attach);
         Body.addBodyPart(attBodyPart);
 
-        text = "<br><br><a href='url'>Approve this document</a><br>";
-        text += "<a href='url'>Deny this document</a><br>";
 
-        textBodyPart.setContent(text, "text/html; charset=utf-8");
-        Body.addBodyPart(textBodyPart);
+        text = "<br><br><a href='https://en.wikipedia.org/wiki/Cat'>Approve this document</a><br>";
+        text += "<a href='https://en.wikipedia.org/wiki/Cat'>Deny this document</a><br>";
+
+        MimeBodyPart textBodyPart2 = new MimeBodyPart();
+        textBodyPart2.setContent(text, "text/html; charset=utf-8");
+        Body.addBodyPart(textBodyPart2);
         return Body;
+    }
+
+    public static void main(java.lang.String[] args) {
+        File pic = new File("out/production/Emails/arm.png");
+        String name = "FLEX GUIDE";
+
+        emails jeff = new emails();
+        jeff.sendEmail(pic, name);
+
     }
 }
